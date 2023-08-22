77c4007f0696e960a75b9520cc15e438295a9863
omprakashmeda <omeda@voicenation.com>
2023-08-22 10:45:14 +0530

    - Clone sim_info repository

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+## 0.1.3
+- Fixed library bug
+## 0.1.2
+- Android Permission Related Updates
+## 0.1.1
+- Update README.md
+## 0.1.0
+- Add Plugin Test Code
+- Delete a platform branch of a method with the same name
+
+## 0.0.1
+* Initial Release
---
+++ android/src/main/java/flutter/moum/sim_info/SimInfoPlugin.java
@@ -0,0 +1,38 @@
+package flutter.moum.sim_info;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** SimInfoPlugin */
+public class SimInfoPlugin implements FlutterPlugin {
+  private MethodChannel channel;
+
+
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    SimInfoPlugin plugin = new SimInfoPlugin();
+    plugin.setMethodChannel(registrar.context(), registrar.messenger());
+  }
+
+  private void setMethodChannel(Context context, BinaryMessenger messenger) {
+    channel = new MethodChannel(messenger, "flutter.moum.sim_info");
+    channel.setMethodCallHandler(new MethodHandlerImpl(context));
+
+  }
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    final Context context = binding.getApplicationContext();
+    setMethodChannel(context, binding.getBinaryMessenger());
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+}
